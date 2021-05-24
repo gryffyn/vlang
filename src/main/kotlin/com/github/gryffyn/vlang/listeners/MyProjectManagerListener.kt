9a909a397e451035beb2a668aca2443a3c96d096
GitHub Action <action@github.com>
2021-05-24 19:36:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/gryffyn/vlang/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gryffyn/vlang/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gryffyn.vlang.listeners
 
+import com.github.gryffyn.vlang.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
